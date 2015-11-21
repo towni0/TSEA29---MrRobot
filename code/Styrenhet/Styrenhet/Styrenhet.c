383e55cc5830c62a1fbb01511f241c41796cbffc
towni0 <johan.olin@me.com>
2015-11-21 15:05:41 +0100

    Uppdaterat styrenhetet koden

--- code/Styrenhet/Styrenhet/Styrenhet.c
+++ code/Styrenhet/Styrenhet/Styrenhet.c
@@ -14,15 +14,18 @@
 #define DIR2 PIND2
 #define DIR_PWM_PORT PORTD
 
-#define LASER_PIN PIND1
-#define LASER_PORT PORTD
+#define LASER_PIN PINB4
+#define LASER_PORT PORTB
 
 #define IR_PWM PINB5
 #define IR_PORT PINB
 
-#define LED1 PINBX
-#define LED2 PINBX
-#define LED3 PINBX
+#define LED1_PIN PINB0
+#define LED2_PIN PINB1
+#define LED3_PIN PINB2
+#define INVISIBLE_LED_PIN PINB3
+#define LASER_LED_PIN PINB4
+#define LED_PORT PORTB
 
 //Orders
 #define DO_NOTHING 						0
@@ -68,6 +71,10 @@ int dutyCycle = period* 0.5; // 50% duty cycle to start
 // Health stuff
 int health = 3;
 
+// IR-sender
+bool IRisActivive = true;
+
+
 
 uint8_t currentOrder = 0;
 
@@ -97,6 +104,12 @@ int main(void){
 	//enable receive interrupt
 	UCSR0B |= (1<<RXCIE0);
 	//#UART INITS END#//
+	
+	// Call all Init functions in this module
+	Init();
+	
+	
+	// Activate LOOP HERE
 
     while(1)
     {
@@ -150,21 +163,39 @@ int main(void){
 		case DECREMENT_LED_LIVES:
 			DecrementLEDLives();
 			break;		 
-			
-		case DECREMENT_LED_LIVES:
+		
+		case TURN_INVISIBLE_AND_DEC_LIFE_LED:
+			DecrementLEDLives();
+			TurnOffIRSignature();
+			break;	
+		
+		case RESET_SE:
 			ResetSE();
-			break;		 
-			
+			break;	
 		
 		default:
 			// Error
 			break;	
+			
+			
+			
+			
+			
     }
+	
+	
 }
 
 // Reset all neccesary data
 void ResetSE() {
+	// Reset health
 	health = 3;
+	
+	// Make sure that we dont move when we have restarted this module
+	MoveForward(0);
+	
+	// Set all heath LEDs activte
+	LED_PORT |= (1<<LED1_PIN) | (1<<LED2_PIN) | (1<<LED3_PIN);
 }
 
 // Set doutyCycle for the PWM pins
@@ -173,7 +204,18 @@ void SetPWM() {
 	OCR1B = ICR1 - dutyCycle; // duty cycle on "dutyCycle" of length "period" for PD4
 }
 
+// Calls all Init functions
+void Init() {
+	InitPWM();
+	InitLEDs();
+}
 
+// Set all LED pins as output and light them up!
+void InitLEDs() {
+	DDRB |= (1<<LED1_PIN) | (1<<LED2_PIN) | (1<<LED3_PIN) | (1<<INVISIBLE_LED_PIN) | (1<<LASER_LED_PIN);
+	// Set all heath LEDs activte
+	LED_PORT |= (1<<LED1_PIN) | (1<<LED2_PIN) | (1<<LED3_PIN);
+}
 
 // Setup of PWM and DIR
 void InitPWM() {
@@ -213,23 +255,42 @@ void TurnRight(int speed) {
 	DIR_PWM_PORT |= (1<<DIR1);
 }
 
-// Activates the laser pointer
+// Activates the laser pointer and the Laser lED
 void ActivateLaser() {
+	LASER_PORT |= (1<<LASER_PIN);
 
+}
+
+// Deactivates the laser pointer and the Laser LED
+void DeactivateLaser() {
+	LASER_PORT &= ~(1<<LASER_PIN);
 }
 
 // Turns the IR-sender off (invisible)
 void TurnOffIRSignature() {
-	
+	IRisActivive = false;
 }
 
 // Turns the IR-sender on (not invisible)
 void TurnOnIRSignature() {
-	
+	IRisActivive = true;
 }
 
 // Decrement the amount of lives we have (show on less LED)
 void DecrementLEDLives() {
+	health--;
+	
+	if (health == 2) {
+		LED_PORT &= ~(1<<LED3_PIN);
+	}
+	
+	if (health == 1) {
+		LED_PORT &= ~(1<<LED2_PIN);
+	}
+	
+	if (health == 0) {
+		LED_PORT &= ~(1<<LED1_PIN);
+	}
 	
 }
 
@@ -240,7 +301,7 @@ void StopMove() {
 }
 
 
-
+// UART ISR which sets the "currentOrder" variable
 ISR(USART0_RX_vect){
 	uint8_t messageID = UDR0 & 0x07;
 	//only look at ORDERS
